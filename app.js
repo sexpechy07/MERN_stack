636fc3d50f381981db4f54daa1f8f780584b8ec3
MEDINA RODRIGO Borja <MEDINABO@CDM.CM-CIC.FR>
2022-07-26 13:32:16 +0200

    connection mongoDB

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 // app.js
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+connectDB();
+
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to Borja Medina application." });
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 });
