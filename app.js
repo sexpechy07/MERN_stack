4bafc0e93c0cc014d9be9671201b3e78cf2fe05a
Borja Medina <borjamedina07@gmail.com>
2022-08-05 19:42:59 +0200

    frontend

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 // app.js
+
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+// Connect Database
 connectDB();
 
-app.get('/', (req, res) => {
-    res.json({ message: "Welcome to Borja Medina application." });
-});
+// cors
+app.use(cors({ origin: true, credentials: true }));
 
-const port = process.env.PORT || 8081;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-});
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
